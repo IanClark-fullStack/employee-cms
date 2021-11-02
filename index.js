f98089edade77daf5ddf29f5849c58391a9b836c
Ian Clark <IanClark2021@u.northwestern.edu>
2021-11-01 23:14:19 -0500

    updates to come

--- index.js
+++ index.js
@@ -20,14 +20,6 @@ const startUpOptions = [
         value: 'VIEW_ROLES'
     },
     {
-        name: 'View all employees by department',
-        value: 'VIEW_EMPLOYEES_BY_DEPARTMENT'
-    },
-    {
-        name: 'View all employees by manger',
-        value: 'VIEW_EMPLOYEES_BY_MANGER'
-    },
-    {
         name: 'Add an employee',
         value: 'ADD_EMPLOYEE'
     },
@@ -138,23 +130,35 @@ const sendStartUp = () => {
     inquirer.prompt(startPrompt)
     .then(({ startUpData }) => {
         switch (startUpData) {
-            case 'View all departments':
+            case 'VIEW_DEPARTMENTS':
                 viewDepartments();
                 break;
-            case 'View all employees':
+            case 'VIEW_EMPLOYEES':
                 viewEmployees();
                 break;
-            case 'Add a department':
+            case 'ADD_DEPARTMENT':
                 addDepartment();
                 break;
-            case 'Add a role': 
+            case 'REMOVE_DEPARTMENT':
+                removeDepartment();
+                break;
+            case 'ADD_ROLE': 
                 addRole();
                 break;
-            case 'Add an employee':
+            case 'ADD_ROLE':
                 addEmployee();
                 break;
-            case 'Update an employee role':
+            case 'UPDATE_ROLE':
                 updateRole();
+                break;
+            case 'UPDATE_EMPLOYEE':
+                updateEmployee();
+                break;
+            case 'VIEW_ROLES':
+                viewRoles();
+                break;
+            case 'REMOVE_ROLES':
+                removeRole();
                 break;
             case 'End':
                 db.end();
@@ -166,7 +170,7 @@ const sendStartUp = () => {
 // **************** View Departments *** READ All, select * from
 const viewEmployees = () => {
     console.log(chalk.blue.underline.bold('*****  A L L  D E P A R T M E N T S  *****\n'));
-    db.query(`SELECT employee.id, employee.first_name, employee.last_name , job_role.title, department.department_name AS 'department', job_role.salary, CONCAT(manager.first_name, " " , manager.last_name) AS 'manager' FROM employee LEFT JOIN job_role ON employee.job_role_id = job_role.id LEFT JOIN department ON job_role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;`, (err, res) => {
+    db.query(`SELECT * FROM employee`, (err, res) => {
         if (err) { console.log(err); }
         else { 
             console.table(res);
@@ -175,8 +179,64 @@ const viewEmployees = () => {
        
     })
 };
+const updateEmployee = () => {
+    db.query(`SELECT employee.id, first_name, last_name FROM employee;`, (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const employee = res.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.id}));
+            
+            db.query(`SELECT job_role.id, job_role.title FROM job_role;`, (err, res) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    const role = res.map(role => ({name: job_role.title, value: job_role.id}));
+
+                    prompt([
+                        {
+                            type: 'list',
+                            name: 'employee',
+                            message: 'Select the employee',
+                            choices: employee
+                        },
+                        {
+                            type: 'list',
+                            name: 'role',
+                            message: 'Select the new role',
+                            choices: role
+                        }
+                    ])
+
+                    .then(res => {
+                        db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [res.role, res.employee], (err, res) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                sendStartUp();
+                            }
+                        })
+                    })
+                }
+            })
+        }
+    })
+};
+
 
 // **************** ADD Departments *** INSERT INTO department SET name
+const viewDepartments = () => {
+    db.query(`SELECT department.id, department.department_name FROM department;`, (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(res);
+           sendStartUp();
+        }
+    })
+
+}
+
+
 const addDepartment = () => {
     inquirer.prompt(departmentAdd)
     .then((departmentData) => {
@@ -185,7 +245,7 @@ const addDepartment = () => {
         // Push the New Department to an Array to feed into choices 
         departmentList.push(departmentToAdd);
         // Insert INTO Deparment Table
-        db.query(`INSERT INTO department(department_name) VALUE ?`, departmentData.department_name, (err, res) => {
+        db.query(`INSERT INTO department(department_name) SET ?`, departmentData.department_name, (err, res) => {
             if (err) { console.log(err); }
             else { sendStartUp(); }  
         })
@@ -222,7 +282,7 @@ const removeDepartment = () => {
 
 // **************** ROLES Functionality *** View, Add and Update
 const viewRoles = () => {
-    db.query(`SELECT role.id, role.title, department.department_name AS 'department', role.salary FROM role INNER JOIN department ON role.department_id = department.id;`, (err, res) => {
+    db.query(`SELECT job_role.id, job_role.title, department.department_name AS 'department', job_role.salary FROM job_role INNER JOIN department ON job_role.department_id = department.id;`, (err, res) => {
         if (err) {
             console.log(err);
         } else {
@@ -265,6 +325,7 @@ const addRole = () => {
         }
     })
 };
+
             
         
     
